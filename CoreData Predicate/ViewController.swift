839a8cf1ec70e5bb16fab445203ee628c13a5db7
arsenium313 <arsenium313@gmail.com>
2022-12-21 15:33:09 +0300

    Initial Commit

---
+++ CoreData Predicate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData Predicate
+//
+//  Created by Арсений Кухарев on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
